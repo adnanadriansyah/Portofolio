774573086a5362cd3d7a87ee8dafc05cb43b17bc
adnanadriansyah <adnanadriansyah5@gmail.com>
2026-05-26 11:07:38 +0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./*.html"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ['Playfair Display', 'Georgia', 'serif'],
+        sans:  ['Outfit', 'sans-serif'],
+        mono:  ['JetBrains Mono', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+}
